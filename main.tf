99cf209add249d8a65cb19c8af4ac4e8822b17a9
nagamani <nagamanichavaaws@gmail.com>
2023-03-15 11:15:00 +0000

    des

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_autoscaling_group" "asg" {
   name                      = "${var.component}-asg"
   max_size                  = var.max_size
   min_size                  = var.min_size
-  desired_capacity          = value.desired_capacity
+  desired_capacity          = var.desired_capacity
   force_delete              = true
   vpc_zone_identifier       = var.subnet_ids
 
