e16cb1d4d701367be8932107047c9c7742eb1154
nagamani <nagamanichavaaws@gmail.com>
2023-03-17 11:13:48 +0000

    ami

--- data.tf
+++ data.tf
@@ -4,8 +4,14 @@ data "aws_kms_key" "key" {
   key_id = "alias/roboshop"
 }
 
+# data "aws_ami" "centos8" {
+#   most_recent = true
+#   name_regex  = "Centos-8-DevOps-Practice"
+#   owners      = ["973714476881"]
+# }
+
 data "aws_ami" "centos8" {
   most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
+  name_regex  = "ansible-installed"
+  owners      = ["633788536644"]
 }
