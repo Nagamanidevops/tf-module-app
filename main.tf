cb3486535ec549e78cdb5fbee41a629bb699c015
nagamani <nagamanichavaaws@gmail.com>
2023-04-01 14:06:20 +0000

    a

--- main.tf
+++ main.tf
@@ -84,7 +84,9 @@ resource "aws_security_group" "main" {
             "arn:aws:ssm:us-east-1:998626143474:parameter/nexus*",
            # "arn:aws:ssm:us-east-1:633788536644:parameter/${var.env}.docdb*"
             "arn:aws:ssm:us-east-1:998626143474:parameter/${var.env}.docdb*",
-            "arn:aws:ssm:us-east-1:998626143474:parameter/${var.env}.elasticache*"
+            "arn:aws:ssm:us-east-1:998626143474:parameter/${var.env}.elasticache*",
+            "arn:aws:ssm:us-east-1:998626143474:parameter/${var.env}.rds*"
+
 
 
 
