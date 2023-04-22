8e880a11d33e04949422f6f50b0ba462609cc48d
nagamani <nagamanichavaaws@gmail.com>
2023-04-22 07:56:16 +0000

    port

--- main.tf
+++ main.tf
@@ -247,8 +247,8 @@ resource "aws_lb_listener" "frontend" {
 resource "aws_lb_listener" "frontend_http" {
   count = var.listener_priority == 0 ? 1 : 0
   load_balancer_arn = var.alb_arn
-  port              = "443"
-  protocol          = "HTTPS"
+  port              = "80"
+  protocol          = "HTTP"
   
 
   default_action {
